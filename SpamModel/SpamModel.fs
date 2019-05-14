577d4d1b9a8b59f689cb05c9db5f057ffeebc840
TimJLockwood <30637209+TimJLockwood@users.noreply.github.com>
2019-05-14 19:34:44 +0100

    Reverted SpamModel changes

--- SpamModel/SpamModel.fs
+++ SpamModel/SpamModel.fs
@@ -52,15 +52,11 @@ module spamModel =
     // Check if email is spam. Returns simple boolean
     // subject and bodyText should be free of quotation marks and other characters that could escape the string.
     let isSpam (predictor:PredictionEngine<EmailData, EmailResult>) (subject:string) (bodyText:string) = 
-        true
-        //let prediction = predictor.Predict({HamOrSpam = false; Subject = subject; BodyText = bodyText})
-        //true
-        (*
+        let prediction = predictor.Predict({HamOrSpam = false; Subject = subject; BodyText = bodyText})
         // Debug data
         #if DEBUG
         subject |> printfn "DEBUG: Subject: %s"
         bodyText |> printfn "DEBUG: Body Text: %s"
         prediction.Probability |> printfn "DEBUG: Probability: %%%A -- WIP"
         #endif
-        prediction.PredictedLabel = true
-        *)
+        prediction.PredictedLabel
