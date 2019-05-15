f1b32cebc295c81ef3cb642e4fe1b0edef613486
TimJLockwood <30637209+TimJLockwood@users.noreply.github.com>
2019-05-15 23:08:08 +0100

    Initial prep for ml tests

--- SpamFilterTest/SpamModelTest.fs
+++ SpamFilterTest/SpamModelTest.fs
@@ -1,8 +1,13 @@
 namespace SpamModelTest
 module SpamModelTest =
     open NUnit.Framework
+    open Microsoft.ML
+    open Microsoft.ML.Data
     open SpamModel
     open FsCheck
+
+    [<SetUp>]
+    let mlContext = MLContext()
 
     [<Test>]
     let ``I have a descriptive test name`` () =
