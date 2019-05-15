6e26e30de3938b7ba29c48e4760d8c6fb36c9681
TimJLockwood <30637209+TimJLockwood@users.noreply.github.com>
2019-05-15 02:13:43 +0100

    added initial build scripts

--- MailCore/MailCore.fs
+++ MailCore/MailCore.fs
@@ -2,24 +2,21 @@
 module fsMessages =
     open Microsoft.Office.Interop
     open SpamModel.spamModel
+    open SpamModel
+    
+    open Microsoft.ML
+    open Microsoft.ML.Data
+    open Microsoft.ML.Transforms
+    open Microsoft.ML.Trainers
 
-    //let predictionEngine = loadModelAndGetPredictor <| "spamModel"
     let checkIsSpam (mail:Outlook.MailItem, outlookNamespace:Outlook.NameSpace, predictionEngine:Microsoft.ML.PredictionEngine<EmailData, EmailResult>) =
         match mail.MessageClass with
         | "IPM.Note" ->
             if mail.Body |> (mail.Subject |> (predictionEngine|> isSpam))
-            then mail.Move(outlookNamespace.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderJunk)) |> ignore
-                 ()
-            else ()
+            then 
+                mail.Move(outlookNamespace.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderJunk)) |> ignore
+                ()
+            else 
+                ()
         | _ ->
             ()
-
-    let test (mail:Outlook.MailItem) = 
-        match mail.MessageClass with
-        | "IPM.Note" ->
-            if mail.Body |> (mail.Subject |> ((loadModelAndGetPredictor <| "spamModel")|> isSpam))
-            then 
-                //mail.Move(nameSpace.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderJunk)) |> ignore
-                ignore
-            else ignore
-        | _ -> ignore
---
+++ OutlookPlugin/MoverBat.bat
@@ -0,0 +1,1 @@
+xcopy /y spamModel %TEMP%
