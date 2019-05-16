ff6e5c5be927b30c49451b2bfa0bebf646ee96c1
KinseyKid <KinseyKid@users.noreply.github.com>
2019-05-16 09:45:05 +0100

    Added Comments to MailCore and SpamModel

--- MailCore/MailCore.fs
+++ MailCore/MailCore.fs
@@ -2,18 +2,19 @@
 module fsMessages =
     open Microsoft.Office.Interop
     open SpamModel.spamModel
-    open SpamModel
+    open TextCleaner
     
-    open Microsoft.ML
-    open Microsoft.ML.Data
-    open Microsoft.ML.Transforms
-    open Microsoft.ML.Trainers
-
     let checkIsSpam (mail:Outlook.MailItem, outlookNamespace:Outlook.NameSpace, predictionEngine:Microsoft.ML.PredictionEngine<EmailData, EmailResult>) =
+        //This is a match expression effectively checking if the MailItem/COM object that has been passed to this method is an email or not, Emails have a .MessageClass value of IPM.Note
+        //MessageClass Types API: https://docs.microsoft.com/en-us/office/vba/outlook/concepts/forms/item-types-and-message-classes
+        //MailItem.MessageClass API: https://docs.microsoft.com/en-us/office/vba/api/outlook.mailitem.messageclass
         match mail.MessageClass with
         | "IPM.Note" ->
-            if mail.Body |> (mail.Subject |> (predictionEngine|> isSpam))
+            //Calls Wills Textcleaner with the value mail.Body and then mail.subject which is string
+            //The cleaned strings are then piped into the isSpam method which will return a true or false value determining whether or not an email is spam
+            if textCleaner.fullParse(mail.Body) |> (textCleaner.fullParse(mail.Subject) |> (predictionEngine|> isSpam))
             then 
+                //Moves a Mailitem object (an email) to the default junk folder
                 mail.Move(outlookNamespace.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderJunk)) |> ignore
                 ()
             else 
--- OutlookPlugin/ThisAddIn.cs
+++ OutlookPlugin/ThisAddIn.cs
@@ -13,7 +13,7 @@ namespace OutlookPlugin
 {
     public partial class ThisAddIn
     {
-        // Instantiation
+        // Instantiation of OutlookNamespace, inbox, items and Prediction engine
         Outlook.NameSpace outlookNameSpace;
         Outlook.MAPIFolder inbox;
         Outlook.Items items;
@@ -21,13 +21,23 @@ namespace OutlookPlugin
 
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+            // OutlookNamespace is a reference to the instance of outlook the add-in is running in, this is acquired through GetNameSpcae("MAPI")
             outlookNameSpace = this.Application.GetNamespace("MAPI");
+
+            //API doc : https://docs.microsoft.com/en-us/dotnet/api/microsoft.office.interop.outlook.oldefaultfolders?view=outlook-pia
+            //inbox is a reference to the default folder which is acquired using an enumeration of OlDefaultFolders
+            //API doc for outlookNameSpace.getDefaultFolder : https://docs.microsoft.com/en-us/office/vba/api/outlook.namespace.getdefaultfolder
             inbox = outlookNameSpace.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderInbox);
+
+            //predictionEngine is an instance of the prediction engine that is being used in SpamModel by being passedto MailCore and then to SpamModel
             predictionEngine = spamModel.loadModelAndGetPredictor("spamModel");
 
+            //gets a collection of items from specified folder, in this case the default folder we previously found
             items = inbox.Items;
+            //When Items are added to the inbox that was previously "got" a new item event occurs and the event handler calls the method Items_ItemAdd
             items.ItemAdd +=
                 new Outlook.ItemsEvents_ItemAddEventHandler(Items_ItemAdd);
+
 
             this.Startup += new System.EventHandler(ThisAddIn_Startup);
             this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
@@ -35,7 +45,10 @@ namespace OutlookPlugin
 
         private void Items_ItemAdd(object Item)
         {
+            //This sets the type of the parameter that has been passed to an outlook mail item which means the properties of a mail item can be used on it.
             Outlook.MailItem mail = (Outlook.MailItem)Item;
+            //This is a call to the MailCore library with the following parameters being passed; The MailItem object, the Outlook namespace and the spam model we previously created.
+            //This is so there does need to be anything else programmed in C# and all of the email moving and predictions can be done in F#
             fsMessages.checkIsSpam((Outlook.MailItem)Item, (Outlook.NameSpace)outlookNameSpace,(Microsoft.ML.PredictionEngine<spamModel.EmailData, spamModel.EmailResult>)predictionEngine);
         }
 
