dbc65bbac3299c4d500c15fc250cb208defe73dc
TimJLockwood <30637209+TimJLockwood@users.noreply.github.com>
2019-05-16 11:48:02 +0100

    Removed emailCore tests

--- SpamFilterTest/MailCoreTest.fs
+++
@@ -1,9 +0,0 @@
-namespace MailCoreTest
-module MailCoreTest =
-    open NUnit.Framework
-    open MailCore
-    open FsCheck
-
-    [<Test>]
-    let ``I have a descriptive test name`` () =
-        Assert.Pass()
