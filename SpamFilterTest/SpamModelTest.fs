8f884ea44960722c0d79040981045e17317b2b34
TimJLockwood <30637209+TimJLockwood@users.noreply.github.com>
2019-05-16 10:29:50 +0100

    Added spamModel tests

--- SpamFilterTest/SpamModelTest.fs
+++ SpamFilterTest/SpamModelTest.fs
@@ -2,13 +2,33 @@ namespace SpamModelTest
 module SpamModelTest =
     open NUnit.Framework
     open Microsoft.ML
-    open Microsoft.ML.Data
-    open SpamModel
+    open SpamModel.spamModel
     open FsCheck
 
     [<SetUp>]
     let mlContext = MLContext()
+    let pathToModel = "../../../../OutlookPlugin/spamModel"
+    let pathToMissingModel = "spamModel"
+    let predictionEngine = pathToModel |> loadModelAndGetPredictor
 
     [<Test>]
-    let ``I have a descriptive test name`` () =
-        Assert.Pass()
+    let ``LoadSchemaAndGetPredictor model exists`` () =
+        let input = pathToModel
+        let result = input |> loadModelAndGetPredictor
+        result |> Assert.IsInstanceOf<PredictionEngine<SpamModel.spamModel.EmailData, SpamModel.spamModel.EmailResult>>
+    
+    [<Test>]
+    let ``LoadSchemaAndGetPredictor model missing`` () =
+        let input = pathToMissingModel
+        try
+            input |> loadModelAndGetPredictor |> ignore
+            Assert.Fail ()
+        with
+            | :? System.IO.FileNotFoundException -> Assert.Pass ()
+
+    [<Test>]
+    let ``IsSpam check return type`` () =
+        let inputA = "Hello"
+        let inputB = "World"
+        let result = inputB |> (inputA |> (isSpam predictionEngine))
+        result |> Assert.IsInstanceOf<bool>
